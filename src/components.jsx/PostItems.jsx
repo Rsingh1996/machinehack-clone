64ca2cab8da4d3a1b5c99edc4085e1fdb46d2b2f
Rahul <rahulna1996@gmail.com>
2023-09-30 21:26:20 +0530

    Updated Add component

--- src/components.jsx/PostItems.jsx
+++ src/components.jsx/PostItems.jsx
@@ -1,6 +1,21 @@
 const PostItems = [
   {
     id: 1,
+    avatar: "https://cdn.machinehack.com/default/M.gif",
+    author: "MachineHack Team",
+    postedDate: "September 30, 2023",
+    postContent: `MachineHack is an exceptional professional network specifically tailored to empower and showcase the distinct abilities and skills of contemporary generative AI professionals. In addition to its vibrant community, MachineHack offers an extensive array of data-centric challenges and competitions. These engaging opportunities enable generative AI professionals to evaluate and refine their skills while gaining valuable insights from fellow members in the community. By fostering collaboration and knowledge sharing, MachineHack serves as a catalyst for the growth and advancement of generative AI professionals in this rapidly evolving field
+    `,
+    image:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F149695847.v2.pressablecdn.com%2Fwp-content%2Fuploads%2F2022%2F11%2Fimage-19-1-1300x650.png&f=1&nofb=1&ipt=17449af7ba7be76254c5a14a7b7778aa220297f2da01b0c6f41d629cbb29a4d5&ipo=images",
+
+    like: 10,
+    comment: 5,
+    bookmarks: 0,
+    share: 1,
+  },
+  {
+    id: 2,
     avatar: "https://cdn.machinehack.com/default/B.gif",
     author: "Bob Justin",
     postedDate: "September 14, 2023",
@@ -27,7 +42,7 @@ const PostItems = [
     share: 0,
   },
   {
-    id: 2,
+    id: 3,
     avatar: "https://cdn.machinehack.com/default/S.gif",
     author: "SIDDHARTH KUMAR",
     postedDate: "September 24, 2023",
@@ -42,7 +57,7 @@ const PostItems = [
     share: 0,
   },
   {
-    id: 3,
+    id: 4,
     avatar: "https://cdn.machinehack.com/default/S.gif",
     author: "SUMANTA BASAK",
     postedDate: "August 27, 2023",
@@ -56,7 +71,21 @@ const PostItems = [
     share: 0,
   },
   {
-    id: 4,
+    id: 5,
+    avatar: "https://cdn.machinehack.com/default/M.gif",
+    author: "Mohal Lal",
+    postedDate: "September 29, 2023",
+    postContent: `How to participate as a team for this hackathon? Should each person register independently? 
+    `,
+    image: "",
+
+    like: 0,
+    comment: 0,
+    bookmarks: 0,
+    share: 0,
+  },
+  {
+    id: 6,
     avatar: "https://cdn.machinehack.com/default/K.gif",
     author: "Karam Janit Shandilya",
     postedDate: "September 4, 2023",
@@ -65,6 +94,96 @@ const PostItems = [
     `,
     image:
       "https://machinehack-be.s3.ap-south-1.amazonaws.com/feed/629e02ed97aeb5aeb19b0e6e/Machine_Hack__Events__uploaded_at_1692948970432.png",
+
+    like: 0,
+    comment: 0,
+    bookmarks: 0,
+    share: 0,
+  },
+  {
+    id: 7,
+    avatar: "https://cdn.machinehack.com/default/K.gif",
+    author: "Nayata Kukreja",
+    postedDate: "September 4, 2023",
+    postContent: `Unleash Exclusive Hackathon Superpowers! Get closer to winning $3,500
+
+    Fill out this Google Form with the link to your LinkedIn post: https://forms.gle/QETU3nhQxqNz9p3HA 
+    `,
+    image:
+      "https://machinehack-be.s3.ap-south-1.amazonaws.com/feed/6479bbedc0182954740fe1ca/Machine_Hack__Events__uploaded_at_1689071322298.jpeg",
+
+    like: 0,
+    comment: 0,
+    bookmarks: 0,
+    share: 0,
+  },
+  {
+    id: 8,
+    avatar: "https://cdn.machinehack.com/default/J.gif",
+    author: "Jeevan Reddy",
+    postedDate: "September 30, 2023",
+    postContent: `How should I start learning ML?
+    `,
+    image: "",
+
+    like: 1,
+    comment: 1,
+    bookmarks: 0,
+    share: 0,
+  },
+  {
+    id: 9,
+    avatar: "https://cdn.machinehack.com/632830f74467e747f30b46ff/blob",
+    author: "Sourabh Mehta",
+    postedDate: "September 30, 2023",
+    postContent: `Language models are AI systems designed to understand and generate human-like text. Rather than providing a direct answer or solution, LLMs follow a logical chain of thoughts, simulating human-like reasoning processes.
+    https://machinehack.com/story/conversational-symphony-decoding-the-chain-of-thoughts-in-llms
+    `,
+    image:
+      "https://machinehack-be.s3.ap-south-1.amazonaws.com/feed/632830f74467e747f30b46ff/Machine_Hack__Events__uploaded_at_1685420217709.jpeg",
+
+    like: 5,
+    comment: 3,
+    bookmarks: 0,
+    share: 1,
+  },
+  {
+    id: 10,
+    avatar: "https://cdn.machinehack.com/default/J.gif",
+    author: "Pritam Sinha",
+    postedDate: "September 30, 2023",
+    postContent: `What do the negative COTTON AREA and COTTON PRODUCTION values mean in Indian data? Can it be negative?
+    `,
+    image: "",
+
+    like: 0,
+    comment: 0,
+    bookmarks: 0,
+    share: 0,
+  },
+  {
+    id: 11,
+    avatar: "https://cdn.machinehack.com/default/H.gif",
+    author: "Hitesh Rao",
+    postedDate: "September 20, 2023",
+    postContent: `When will the workshop regarding use of vertex ai in the competition take place?What are the dates for the same?Give some idea please.
+    `,
+    image: "",
+
+    like: 1,
+    comment: 1,
+    bookmarks: 0,
+    share: 0,
+  },
+  {
+    id: 12,
+    avatar: "https://cdn.machinehack.com/default/A.gif",
+    author: "Abhirama G",
+    postedDate: "September 10, 2023",
+    postContent: `The dataset mentions that the last columns are information about other fertilizers, but in the sample submission notebook, it's showing the columns as total fertilizer consumption.
+    Anyone, any views on this ?
+    `,
+    image: "",
 
     like: 0,
     comment: 0,
